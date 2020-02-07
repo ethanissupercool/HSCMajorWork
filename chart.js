221a66630920da177cd59ca2749d2410c1d7364e
ethan <wizardcat@pm.me>
2020-02-07 13:24:16 +1100

    Update
    - Added UI elements such as spending catagories

--- chart.js
+++ chart.js
@@ -1,12 +1,12 @@
 new Chart(document.getElementById("doughnut-chart"), {
     type: 'doughnut',
     data: {
-      labels: ["Home & Utilities", "Insurance & Financial", "Groceries", "Personal & Medical", "Entertainment & Eat Out"],
+      labels: ["Home & Utilities", "Insurance & Financial", "Shopping", "Groceries", "Personal & Medical", "Entertainment & Eat Out", "Transport & Auto", "Children", "Other"],
       datasets: [
         {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          label: "$",
+          backgroundColor: ["#3e95cd", "#a5447c","#daf7b5","#d01258","#a449b2", "#15c343", "#d44e47", "#fc618e", "#b2ea7c"],
+          data: [2478,5267,734,784,433,2342,2352,6462,235]
         }
       ]
     },
